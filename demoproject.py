5ca8f6de43f3fb6b7acb4c5317b3928f031bb60a
JasmanCodes <jasman120105@gmail.com>
2025-02-22 19:28:10 +0530

    demoproject.py

--- demoproject.py
+++ demoproject.py
@@ -5,17 +5,17 @@ def print_board(board):
 
 
 def check_winner(board):
-    # Check rows
+    
     for row in board:
         if row[0] == row[1] == row[2] != ' ':
             return row[0]
     
-    # Check columns
+   
     for col in range(3):
         if board[0][col] == board[1][col] == board[2][col] != ' ':
             return board[0][col]
     
-    # Check diagonals
+    
     if board[0][0] == board[1][1] == board[2][2] != ' ':
         return board[0][0]
     if board[0][2] == board[1][1] == board[2][0] != ' ':
